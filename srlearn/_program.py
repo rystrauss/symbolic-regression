13cf219737078ffb51e4cd01aa7d67b6f7a06efe
Ryan Strauss <rystrauss@davidson.edu>
2019-02-08 00:15:07 -0500

    Add program class

---
+++ srlearn/_program.py
@@ -0,0 +1,101 @@
+"""The underlying data structure used in the genetic programming algorithm.
+
+A program represents one individual in the population and uses prefix notation to
+represent an expression tree.
+
+Author: Ryan Strauss
+Author: Sarah Hancock
+"""
+
+import numpy as np
+
+from .function import Function
+
+
+class _Program:
+    """A prefix notation representation of a program.
+
+    This is the data structure that gets operated on by the genetic algorithm.
+    """
+
+    def __init__(self,
+                 function_set,
+                 max_depth,
+                 const_range,
+                 num_features,
+                 init_method,
+                 program=None):
+        """Constructor.
+
+        Args:
+            function_set (list): A list of valid functions to use in the program.
+            max_depth (int): The maximum allowed depth for the tree when the initialization
+            method is 'grow'. When the initialization method is 'full', this will be the height of the tree.
+            const_range (tuple of two ints): The range of constants to include in the formulas.
+            num_features (int): The number of features.
+            init_method (str):
+                - 'grow': Nodes are chosen at random from both functions and terminals, allowing
+                          for smaller trees than `init_depth` allows. Tends to grow asymmetrical trees.
+                - 'full': Functions are chosen until the `init_depth` is reached, and then terminals are selected.
+                          Tends to grow 'bushy' trees.
+            program (tuple, optional): The prefix notation representation of the program. If None, a new naive
+            random tree will be grown.
+        """
+        if max_depth < 0:
+            raise ValueError('max_depth must be at least zero.')
+        if not isinstance(const_range, tuple) or len(const_range) != 2:
+            raise ValueError('terminal_range must be a 2-tuple.')
+        if num_features < 1:
+            raise ValueError('num_features must be at least 1.')
+        if init_method not in ['grow', 'full']:
+            raise ValueError('"{}" is not a valid init_method.'.format(init_method))
+        for element in function_set:
+            if not isinstance(element, Function):
+                raise ValueError('function_set can only contain elements of type `Function`.')
+
+        self.function_set = function_set
+        self.max_depth = max_depth
+        self.const_range = const_range
+        self.num_features = num_features
+        self.init_method = init_method
+        self.program = program
+
+    def generate_random_program(self, depth):
+        """Recursivley generates a random program.
+
+        Builds a random tree using either the 'full' or 'grow' method, as specified in
+        `self.init_method`. These two techniques are described in [1] and this implementation
+        follows the presentation of the algorithm in [2].
+
+        [1] J. R. Koza, Genetic programming: on the programming of computers by means of natural selection. 1992.
+        [2] W. B. Langdon, R. Poli, N. F. McPhee, and J. R. Koza, “Genetic programming: An introduction and tutorial,
+        with a survey of techniques and applications,” Stud. Comput. Intell., vol. 115, pp. 927–1028, 2008.
+
+        Args:
+            depth (int): The maximum depth of the tree to be created.
+
+        Returns:
+            The prefix notation representation of the generated tree as a tuple.
+        """
+        if depth > self.max_depth:
+            raise ValueError('depth cannot be larger than the maximum depth.')
+
+        term_size = self.num_features + 1
+        func_size = len(self.function_set)
+        # Determine if a function or terminal should be added
+        if self.max_depth == 0 or (
+                self.init_method == 'grow' and np.random.rand() < term_size / (term_size + func_size)):
+            # We need to select a terminal
+            terminal = np.random.randint(term_size)
+            # Potentially select a constant as the terminal
+            if terminal == self.num_features:
+                terminal = np.random.uniform(*self.const_range)
+            program = terminal
+        else:
+            # We need to select a function
+            function = np.random.choice(self.function_set)
+            # Recursivley generated function arguments
+            arguments = [self.generate_random_program(depth - 1) for _ in range(function.arity)]
+            program = (function, *arguments)
+
+        return program
--- srlearn/function.py
+++ srlearn/function.py
@@ -1,23 +1,27 @@
 import numpy as np
 
 
-class _Function:
+class Function:
 
-    def __init__(self, function, arity):
-        if not isinstance(function, callable):
-            raise ValueError('Function must be callable.')
+    def __init__(self, function, name, arity):
+        if not callable(function):
+            raise ValueError('function must be callable.')
+
+        if not (arity == 1 or arity == 2):
+            raise ValueError('arity must be 1 or 2.')
 
         self.function = function
+        self.name = name
         self.arity = arity
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args):
         if len(args) != self.arity:
             raise ValueError('Arity of this function is {}, but {} were provided.'.format(self.arity, len(args)))
         return self.function(*args)
 
 
 def _protected_division(x1, x2):
-    if x2 == 0:
+    if np.abs(x2) < 0.001:
         return 1.
     return np.divide(x1, x2)
 
@@ -46,12 +50,13 @@ def _protected_tan(x):
     return y
 
 
-add = _Function(np.add, 2)
-subtract = _Function(np.subtract, 2)
-multiply = _Function(np.multiply, 2)
-divide = _Function(_protected_division, 2)
-exp = _Function(_protected_exp, 1)
-log = _Function(_protected_log, 1)
-sin = _Function(np.sin, 1)
-cos = _Function(np.cos, 1)
-tan = _Function(_protected_tan, 1)
+add = Function(np.add, 'add', 2)
+subtract = Function(np.subtract, 'subtract', 2)
+multiply = Function(np.multiply, 'multiply', 2)
+divide = Function(_protected_division, 'divide', 2)
+exp = Function(_protected_exp, 'exp', 1)
+log = Function(_protected_log, 'log', 1)
+sin = Function(np.sin, 'sin', 1)
+cos = Function(np.cos, 'cos', 1)
+tan = Function(_protected_tan, 'tan', 1)
+# TODO: Add sqrt
